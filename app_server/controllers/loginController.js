227d4bd59571e0ec58159cc8e1ba79fd8857af0f
Anne Porsgaard <anne@besenbacher.com>
2019-09-21 22:02:25 +0200

    Handle when user doesn't exists

--- app_server/controllers/loginController.js
+++ app_server/controllers/loginController.js
@@ -3,13 +3,11 @@ var mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
 var User = mongoose.model('User');
 
 module.exports.get = function (req, res) {
     res.render('login', { title: 'Express' });
 };
-
 
 module.exports.post = function (req, res) {
     console.log(req.body.inputEmail);
@@ -24,17 +22,26 @@ module.exports.post = function (req, res) {
     User.findOne({
         'emailAddress': newUser.emailAddress
     }, 'emailAddress password', function (err, User) {
-        console.log(User.emailAddress);
-        console.log(User.password);
-        
-        bcrypt.compare(newUser.password, User.password).then(function (result){
-            if(result){
-                console.log('Correct password!');
-                res.redirect('/');
-            }else{
-                console.log('Øv bøv');
-                res.redirect('/login');
-            }
-        })
+        if (User) {
+
+
+            console.log(User.emailAddress);
+            console.log(User.password);
+
+            bcrypt.compare(newUser.password, User.password).then(function (result) {
+                if (result) {
+                    console.log('Correct password!');
+                    res.redirect('/');
+                } else {
+                    console.log('Login error: Incorrect password');
+                    res.redirect('/login');
+                }
+            })
+        }
+        else {
+            console.log('Login error: The user does not exists in the database');
+                    res.redirect('/login');
+        }
     });
+
 }
