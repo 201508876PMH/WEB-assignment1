a9c927d4307d82cf942f95da3324a494fe30815c
andreas <androgles1994@gmail.com>
2020-01-13 09:16:32 +0100

    localhost uri

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://heroku_9x52jczw:k980ff1pgl2iureg04c9dpil79@ds235411.mlab.com:35411/heroku_9x52jczw';
+//var dbURI = 'mongodb://heroku_9x52jczw:k980ff1pgl2iureg04c9dpil79@ds235411.mlab.com:35411/heroku_9x52jczw';
+var dbURI = 'mongodb://127.0.0.1:27017/web-assignment1';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 mongoose.connection.on('connected', () => {
